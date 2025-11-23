69f0394680153001a49574c1c7701d686f0e2643
bapzss <matheusbapcunha@gmail.com>
2025-11-23 11:42:13 -0300

    O db.json foi criado. para a vitrine carregar roda isso aq no terminal: json-server --watch db.json --port 3001 sempre usa o port 3001 pq o react usa o 3000 coloquei tambem a estilização da vitrine no Vitrine.css e coloquei a fonte roboto.

--- src/pages/Vitrine/Vitrine.jsx
+++ src/pages/Vitrine/Vitrine.jsx
@@ -1,6 +1,28 @@
-const Vitrine = (props)=>{
-    return(
-        <h1>ESSA É A VITRINE!</h1>
-    )
+import { useEffect, useState } from "react";
+import "./Vitrine.css";   
+
+export default function Vitrine() {
+    const [produtos, setProdutos] = useState([]);
+
+ useEffect(() => {
+      fetch("http://localhost:3001/produtos")
+    .then((res) => res.json())
+    .then((data) => setProdutos(data));
+}, []);
+
+return (
+    <div className="container">
+      <h1 className="titulo">Vitrine de Produtos</h1>
+
+      <div className="grid">
+        {produtos.map((item) => (
+          <div key={item.id} className="card">
+            <img src={item.imagem} alt={item.nome} />
+            <h3>{item.nome}</h3>
+            <strong>R$ {item.preco}</strong>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-export default Vitrine
